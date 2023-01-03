bf70f491e42d5274410362a267106b015f1450f8
Paul WOISARD <paulwoisard@gmail.com>
2023-01-03 12:43:37 +0100

    régler problème d'icones

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import os
-import sys
 
 os.environ["KIVY_NO_CONSOLELOG"] = "1"
 os.environ["PYTHONPYCACHEPREFIX"] = "$TMPDIR"
 
 from kivy.uix.floatlayout import FloatLayout
 
-import platform
-
 from kivy.app import App
+
+from kivy.config import Config
+Config.set('kivy', 'window_icon', os.path.join("omission", "resources", "icons", "petrol_pump.png"))
 
 from my_kivy.create_uix import kivyUi
 
@@ -21,35 +21,11 @@ class RootWidget(FloatLayout):
         kivyUi(self)
 
 class Low_Fuel(App):
-    if platform.system() == 'Darwin':
-        if hasattr(sys, '_MEIPASS'):
-            icon = '../Resources/petrol_pump.icns'
-        else:
-            icon = 'image/petrol_pump.icns'
-    if platform.system() == 'Linux':
-        icon = 'image/petrol_pump.png'
-    if platform.system() == 'Windows':
-        try:
-            base_path = sys._MEIPASS
-        except Exception:
-            base_path = os.path.abspath(".")
-        icon = os.path.join(base_path, 'image\\petrol_pump.ico')
     def build(self):
         self.title = 'Low-Fuel'
-        if platform.system() == 'Darwin':
-            if hasattr(sys, '_MEIPASS'):
-                self.icon = '../Resources/petrol_pump.icns'
-            else:
-                self.icon = 'image/petrol_pump.icns'
-        if platform.system() == 'Linux':
-            self.icon = 'image/petrol_pump.png'
-        if platform.system() == 'Windows':
-            try:
-                base_path = sys._MEIPASS
-            except Exception:
-                base_path = os.path.abspath(".")
-            self.icon = os.path.join(base_path, 'image\\petrol_pump.ico')
+        self.icon = os.path.join("omission", "resources", "icons", "petrol_pump.png")
         self.root = RootWidget()
+
 
 if __name__ == '__main__':
     Low_Fuel().run()
